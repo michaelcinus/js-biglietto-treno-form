74c31a3cc0d3a5290926c82442c1e0adb294d4cd
Michael Cinus <michaelcinus@gmail.com>
2020-06-17 14:21:30 +0200

    HTML &  JS: FORM and VARIABLE predisposition

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//VARIABLE
+var prezzoKm = 0.21;
+var prezzoTotale;
+var sconto;
+var name = document.getElementById("name");
+var surname = document.getElementById("surname");
+var km = document.getElementById("km");
+var category =  document.getElementById("category");
+var btnBuy = document.getElementById("Buy");
+var btnCancel = document.getElementById("Cancel");
